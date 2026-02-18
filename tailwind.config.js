7cbc7894c3e978ea808c8d1e32b7567f1c3f2169
maggi <gila44dri@gmail.com>
2026-02-18 11:06:19 -0300

    configuracion de mini design system/reglas en el tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#301433",
+        primary: "#341D36",
+        primaryLight: "#5F4661",
+        accent: "#D982E0",
+        muted: "#8B7D8C",
+        soft: "#B188B5",
+        card: "#3A1F3D",
+      },
+      boxShadow: {
+        purpleShadow: "0px 4px 12px rgba(217,130,224,0.25)",
+      },
+      fontFamily: {
+        sans: ["Rubik"],
+      },
+      fontSize: {
+        title: ["28px", { lineHeight: "34px" }],
+        subtitle: ["20px", { lineHeight: "26px" }],
+        body: ["16px", { lineHeight: "24px" }],
+        small: ["13px", { lineHeight: "20px" }],
+      },
+    },
   },
   plugins: [],
 };
